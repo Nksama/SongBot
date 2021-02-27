7af515750c225fb42ebe3bd14a068ac3be6f0574
Nksama <75163978+Nksama@users.noreply.github.com>
2021-02-27 07:51:29 +0530

    Update song.py

--- Bot/modules/song.py
+++ Bot/modules/song.py
@@ -30,7 +30,7 @@ async def song(client, message):
     if args.startswith(" "):
         await message.reply("Enter a song name. Check /help")
         return ""
-    status = await message.reply("Processing...")
+    status = await message.reply("searching that song...")
     video_link = yt_search(args)
     if not video_link:
         await status.edit("Song not found.")
